6b63e460f94e120f8d19a02ca7ca5d83c9d49665
prakritibhatt <prakritibhatt578@gmail.com>
2025-09-13 00:47:53 +0530

    Strings concepts

---
+++ Basics/strings.js
@@ -0,0 +1,6 @@
+const name = "prakriti"
+const repoCount = 50
+console.log(name + repoCount + "Value"); //but now it is not the most effective way to write this way
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
