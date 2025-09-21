db2be4a19bf30221a10ac90e4da0f6fef0d6b332
prakritibhatt <prakritibhatt578@gmail.com>
2025-09-21 20:38:21 +0530

    Immediately invoked function expression

--- Basics_03/iife.js
+++ Basics_03/iife.js
@@ -1,2 +1,8 @@
 //Immediately invoked function expression
-//start here!
+(function coffee(){
+    console.log("Coffee Received!");
+})();          //NOTE: to declare another iife make sure to end the 1st iife fucntion by applying ';'
+
+((name) => {
+    console.log(`Tea Received by ${name}`);  
+})("Praks")
