5c4a94d4f5d77d76c570e0672615414d19ba0a36
prakritibhatt <prakritibhatt578@gmail.com>
2025-09-21 20:31:03 +0530

    Arrow Functions

--- Basics_03/Arrows.js
+++ Basics_03/Arrows.js
@@ -6,3 +6,32 @@ const user = {
     }
 }
 user.welcomeMessage();
+user.username = "Bhatt";
+user.welcomeMessage();
+//console.log(this);  //browser = global --> window, node --> empty object
+
+function forThis(){
+    let username = "praks";
+    console.log(this);   //both console.log() statement gives different output
+    console.log(this.username);    
+    //*Note:* 'this' works differently inside an object and function, in function it cannot be used
+}
+forThis();
+
+//----Arrow Function----
+//basic Arrow Function
+const addTwoNum = (num1,num2) => {
+    return num1+num2;
+}
+console.log(addTwoNum(2,8));
+
+//Implicit return Arrow Function
+const addTwoNum2 = (num1,num2) => (num1 + num2); //feasible for one statement function, also, we use () when we don't want to write return keyword
+console.log(addTwoNum2(1,8));
+
+//For returning object
+const addTwoNum3 = (num1,num2) => ({username : "bhatt"}); 
+console.log(addTwoNum3(1,1));
+
+
+
