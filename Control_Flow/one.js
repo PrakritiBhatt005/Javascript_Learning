5f63b1a19c5cfc4eaa743a59ae7a1782bec5894d
prakritibhatt <prakritibhatt578@gmail.com>
2025-09-22 13:49:50 +0530

    Part One

---
+++ Control_Flow/one.js
@@ -0,0 +1,52 @@
+const isLoggedIn = true;
+const temperature = 41;
+
+if (temperature === 40) {
+    console.log("less than 40");
+} else {
+    console.log("temperature is more than 50");
+    
+}
+
+if ( 2 === "2"){
+    console.log("Executed!");  
+} else{
+    console.log("Not equal");  
+}
+
+const score = 200;
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+//console.log(`User power: ${power}`);
+
+//short hand if
+const balance = 1000;
+if (balance > 500) console.log("Test");
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
