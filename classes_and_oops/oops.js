6467d3d4c6b9ea384376893c93099b4854877ebc
prakritibhatt <prakritibhatt578@gmail.com>
2026-02-19 23:03:20 +0530

    OOPS

--- classes_and_oops/oops.js
+++ classes_and_oops/oops.js
@@ -8,8 +8,20 @@ const user = {
         console.log(`Username is:", ${this.username}`);      
     }
 }
-console.log(user.username);
-console.log(user.getUserDetails());
-console.log(this);          //this...this is different from the above one..both gives different output
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);          //this...this is different from the above one..both gives different output
+
+function User(username, loggedInCount, isLoggedIn){
+    this.username = username;
+    this.loggedInCount = loggedInCount;
+    this.isLoggedIn = isLoggedIn;
+    return this
+}
+const userOne = new User("Prakriti", 2, true)  //new is the constructor function
+const userTwo = new User("Vanshaj", 4, true)  //without you the value of userOne get overwritten by the userTwo
+console.log(userOne);
+console.log(userOne.constructor);   //constructor is a built-in keyword
+console.log(userTwo);
 
 
