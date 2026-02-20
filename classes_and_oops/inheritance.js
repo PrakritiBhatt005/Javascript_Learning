f35f3692e4ddbe7dbdcb91ee552b4e1ab38dc7cd
prakritibhatt <prakritibhatt578@gmail.com>
2026-02-20 20:11:49 +0530

    OOPS

---
+++ classes_and_oops/inheritance.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(username){
+        this.username = username;
+    }
+    logMe(){
+        console.log(`Username is; ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addUser(){
+        console.log("A new user was added...");
+        
+    }
+}
+const userOne = new Teacher("Uno","uno@gmail.com","unn@123")
+userOne.addUser()
---
+++ classes_and_oops/myClasses.js
@@ -0,0 +1,18 @@
+class User{
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    encryptPassword(){
+        return`${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const user1 = new User("Prakriti Bhatt","pk@gmail.com","123")
+console.log(user1.encryptPassword());
+console.log(user1.changeUsername());
+
